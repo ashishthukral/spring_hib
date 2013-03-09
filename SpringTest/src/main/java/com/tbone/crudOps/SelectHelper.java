93e6859a2707e019d1d2ab1c1f3c92309a1b79f2
ashishthukral <yo3287@gmail.com>
2013-03-09 15:11:29 -0500

    minor changes

--- SpringTest/src/main/java/com/tbone/crudOps/SelectHelper.java
+++ SpringTest/src/main/java/com/tbone/crudOps/SelectHelper.java
@@ -8,7 +8,6 @@ import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
@@ -21,9 +20,7 @@ public class SelectHelper {
 
 	public void listStocks() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		Transaction tx = null;
 		try {
-			// tx = session.beginTransaction();
 			Stock stock = (Stock) session.get(Stock.class, 1);
 			LOG.info(stock.getStockName());
 			Criteria criteria = session.createCriteria(Stock.class);
@@ -39,10 +36,7 @@ public class SelectHelper {
 			for (Stock s : list) {
 				LOG.info(s.getStockCode());
 			}
-			// tx.commit();
 		} catch (HibernateException e) {
-			if (tx != null)
-				tx.rollback();
 			LOG.error("Exception Message", e);
 		} finally {
 			session.close();
@@ -51,18 +45,13 @@ public class SelectHelper {
 
 	public void readAll() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		Transaction tx = null;
 		try {
-			tx = session.beginTransaction();
 			List<Stock> stocks = (List<Stock>) session.createQuery("FROM Stock").list();
 			for (Iterator<Stock> iterator = stocks.iterator(); iterator.hasNext();) {
 				Stock aStock = iterator.next();
 				LOG.info(aStock.toString());
 			}
-			tx.commit();
 		} catch (HibernateException e) {
-			if (tx != null)
-				tx.rollback();
 			LOG.error("Exception Message", e);
 		} finally {
 			session.close();
@@ -71,17 +60,12 @@ public class SelectHelper {
 
 	public void readAllCriteria(Class<?> iClass) {
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		Transaction tx = null;
 		try {
-			tx = session.beginTransaction();
 			Criteria criteria = session.createCriteria(iClass);
 			List<?> list = criteria.list();
-			tx.commit();
 			// use toString here explicitly, else works like syso
 			LOG.info(list.toString());
 		} catch (HibernateException e) {
-			if (tx != null)
-				tx.rollback();
 			LOG.error("Exception Message", e);
 		} finally {
 			session.close();
@@ -90,20 +74,15 @@ public class SelectHelper {
 
 	public void readAllCriteriaProjection() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		Transaction tx = null;
 		try {
-			tx = session.beginTransaction();
 			Criteria criteria = session.createCriteria(Stock.class);
 			criteria.setProjection(Projections.projectionList().add(Projections.property("stockId")).add(Projections.property("stockName")));
 			List<Object[]> stocks = (List<Object[]>) criteria.list();
-			tx.commit();
 			for (Iterator<Object[]> itr = stocks.iterator(); itr.hasNext();) {
 				Object[] o = itr.next();
 				LOG.info(Arrays.toString(o));
 			}
 		} catch (HibernateException e) {
-			if (tx != null)
-				tx.rollback();
 			LOG.error("Exception Message", e);
 		} finally {
 			session.close();
