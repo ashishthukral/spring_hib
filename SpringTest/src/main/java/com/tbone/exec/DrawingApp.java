87b6260a7ca01cdae82353d6822b9590cd7e6d5b
ashishthukral <yo3287@gmail.com>
2013-03-09 06:33:06 -0500

    test

--- SpringTest/src/main/java/com/tbone/exec/DrawingApp.java
+++ SpringTest/src/main/java/com/tbone/exec/DrawingApp.java
@@ -6,19 +6,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.tbone.springBeans.Triangle;
 
-
-
 public class DrawingApp {
 
 	private static final Logger LOG = Logger.getLogger(DrawingApp.class);
 
-
 	public static void main(String[] args) {
 		LOG.info("Spring Test start");
-		ApplicationContext context=new ClassPathXmlApplicationContext("/META-INF/spring/spring-context.xml");
-		Triangle aTriangle=(Triangle)context.getBean("triangle");
+		ApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring/spring-context.xml");
+		Triangle aTriangle = (Triangle) context.getBean("triangle");
 		aTriangle.draw();
 		LOG.info(aTriangle);
-		LOG.info("Spring Test end");
+		LOG.info("Spring Test end ");
 	}
 }
